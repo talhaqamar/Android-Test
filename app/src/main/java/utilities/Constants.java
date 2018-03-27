d30af4e121e8dc6b411af5fec686edd5d2e22dc1
talhaqamar <talha.kicsit18@gmail.com>
2018-03-27 11:57:32 +1100

    Constant file will be use to defined constant/global variables.

---
+++ app/src/main/java/utilities/Constants.java
@@ -0,0 +1,11 @@
+package utilities;
+
+/**
+ * Created by talhaqamar on 27/3/18.
+ */
+
+public class Constants {
+
+   public static String URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+
+}
